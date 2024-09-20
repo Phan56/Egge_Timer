3ba65649c3e43227ae7b05a6990f8bddf8dd9aec
Phan56 <13bp@mail.ru>
2024-09-20 23:31:20 +0300

    Happy End

--- ViewController.swift
+++ ViewController.swift
@@ -6,8 +6,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    var timer = Timer()
+    var timeEgg = 0
+    var player: AVAudioPlayer!
+    let stackView = UIStackView()
+    let arreyButton = ["soft_egg" : 3, "medium_egg" : 4, "hard_egg" : 7]
     
     let labelText: UILabel = {
         let label = UILabel()
@@ -19,81 +26,107 @@ class ViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
-    let stackView = UIStackView()
-    
-    let arreyBuuton = ["soft", "medium", "hard"]
-    
-    let testButton: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(named: "hard_egg"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .green
-        return button
-    }()
-    
-    let testButton1: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(named: "hard_egg"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .red
-        return button
-    }()
-    
-    let testButton2: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(named: "hard_egg"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .yellow
-        return button
-    }()
 
+    let progressBar: UIProgressView = {
+        let progress = UIProgressView()
+        progress.progress = 0.0
+        progress.progressViewStyle = .bar
+        progress.progressTintColor = .orange
+        progress.backgroundColor = .gray
+        progress.translatesAutoresizingMaskIntoConstraints = false
+        return progress
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBlue
+        
         cinfigureStackView()
         setupView()
-        
     }
     
     func setupView() {
         view.addSubview(labelText)
         view.addSubview(stackView)
+        view.addSubview(progressBar)
         
-//        labelText.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 15).isActive = true
+        
         labelText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         labelText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         labelText.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
         
-        stackView.topAnchor.constraint(equalTo: labelText.bottomAnchor, constant: 20).isActive = true
         stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-//        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -200).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
-        testButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        testButton.heightAnchor.constraint(equalToConstant: 125).isActive = true
-        
-        testButton1.accessibilityFrame
-        
+        progressBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        progressBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        progressBar.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 100).isActive = true
     }
+    
+    
+    func createButton(named: String) -> UIButton {
+        
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: named), for: .normal)
+        button.setTitle(named, for: .normal)
+        button.setTitleColor(.clear, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.text = named
+        button.addTarget(self, action: #selector(tappedEgge), for: .touchUpInside)
+        return button
+    }
+    
+
     
     func cinfigureStackView() {
         stackView.axis = .horizontal
         stackView.spacing = 10
-//        stackView.distribution = .fillEqually
+        stackView.distribution = .equalSpacing
         stackView.alignment = .center
-        stackView.backgroundColor = .darkGray
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.addArrangedSubview(testButton)
-        stackView.addArrangedSubview(testButton1)
-//        stackView.addArrangedSubview(testButton2)
+        
+        for value in arreyButton.enumerated() {
+//            print("\(value.element.key)")
+            let button = createButton(named: value.element.key)
+            stackView.addArrangedSubview(button)
+            button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 125).isActive = true
+            
+        }
     }
     
     @objc func tappedEgge (_ sender: UIButton) {
         
+        progressBar.progress = 0
+        print(" tipa title \(String(describing: sender.currentTitle)) !!!!")
+        guard let titelButton = sender.currentTitle else {return}
+        guard let timeEgg = arreyButton[titelButton] else {return}
+        self.timeEgg = timeEgg
+        switch titelButton {
+        case "soft_egg": labelText.text = "Soft"
+        case "medium_egg": labelText.text = "Medium"
+        case "hard_egg" : labelText.text = "Hard"
+        default: labelText.text = "How do you like your eggs ???"
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(progressBarUpdate), userInfo: nil, repeats: true)
+        
     }
-
-
+    
+    @objc func progressBarUpdate() {
+        print(progressBar.progress)
+        
+        if progressBar.progress < 1 {
+            progressBar.progress = progressBar.progress + (1 / Float(timeEgg))
+        }else{
+            timer.invalidate()
+            labelText.text = "DONE !!!"
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }
+    }
+    
 }
 
